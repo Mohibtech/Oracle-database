6dec3e5019dc00df1123d7e471add9eb4a2e6e46
Mohib Alvi <mohibtech@gmail.com>
2017-12-07 17:17:50 +0500

    Create Create_Tablespace.sql

---
+++ DynamicSQL/Create_Tablespace.sql
@@ -0,0 +1,34 @@
+set echo      off
+set verify    off
+set feedback  off
+set heading   off
+set head      off
+set linesize  180
+set long  99999
+set pages  0
+set trimspool    on
+set termout      off
+set serveroutput off
+
+spool 'E:\MISC\SQLPLUS\QA-DB_Tblspc.sql'
+select 'select dbms_metadata.get_ddl(''TABLESPACE'','''   ||  tablespace_name || ''') from dual;'   
+from dba_tablespaces;
+
+spool off
+
+BEGIN
+     dbms_metadata.set_transform_param (dbms_metadata.session_transform, 'SQLTERMINATOR', true );
+     dbms_metadata.set_transform_param (dbms_metadata.session_transform, 'PRETTY', true );
+     dbms_metadata.set_transform_param (dbms_metadata.session_transform,'STORAGE' ,false);
+     dbms_metadata.set_transform_param (dbms_metadata.session_transform,'SEGMENT_ATTRIBUTES' , false);
+     dbms_metadata.set_transform_param (dbms_metadata.session_transform,'REF_CONSTRAINTS' , false);
+     dbms_metadata.set_transform_param (dbms_metadata.session_transform,'CONSTRAINTS' , false);
+END;
+
+/
+
+spool 'E:\MISC\SQLPLUS\Final_Tblspc_Create.sql'
+
+@@QA_Tblspc.sql
+
+spool off
