a105ddfe86f8a6b62bbc5ed9d320a67de73e5970
Mohib Alvi <mohibtech@gmail.com>
2018-01-31 13:45:59 +0500

    Update Finding Locks.sql

--- Admin/Finding Locks.sql
+++ Admin/Finding Locks.sql
@@ -6,6 +6,23 @@ from v$locked_object a , v$session b, dba_objects c
 where b.sid = a.session_id
 and   a.object_id = c.object_id
 
+-- Lock Modes expressed descriptively
+SELECT lo.session_id AS sid, s.serial#, 
+       NVL(lo.oracle_username, '(oracle)') AS username,
+       o.owner AS object_owner, o.object_name,
+       Decode(lo.locked_mode, 0, 'None',
+                             1, 'Null (NULL)',
+                             2, 'Row-S (SS)',
+                             3, 'Row-X (SX)',
+                             4, 'Share (S)',
+                             5, 'S/Row-X (SSX)',
+                             6, 'Exclusive (X)', lo.locked_mode) locked_mode,
+       lo.os_user_name
+FROM   v$locked_object lo
+       JOIN dba_objects o ON o.object_id = lo.object_id
+       JOIN v$session s ON lo.session_id = s.sid
+ORDER BY 1, 2, 3, 4;
+
 --Similar to above except using inline tables
 select a.session_id,a.oracle_username, a.os_user_name, b.owner "OBJECT OWNER", b.object_name,b.object_type, a.locked_mode 
 from 
